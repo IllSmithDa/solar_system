eb788ff8b911a37e130bc1dbd8f39f25035ab395
Samuel <sam404kim@gmail.com>
2025-02-01 22:38:19 -0800

    added basic router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home/Home";
 
 const App: React.FC = () => {
 
   return (
-    <Home />
+    <Router>
+      <Routes>
+        < Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
