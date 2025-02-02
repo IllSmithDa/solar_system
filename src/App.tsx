59de7955022b42c40525bc6636024d005ca072ec
Samuel <sam404kim@gmail.com>
2025-02-01 17:01:25 -0800

    testing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 import Home from "./pages/Home/Home";
 
 const App: React.FC = () => {
 
   return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home />}/>
-      </Routes>
-    </Router>
+    <Home />
   );
 };
 
